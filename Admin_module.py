01b199c4bc88955f60c06242c9ffc365f39fcf47
chiomzz <chiomaikerionwu@gmail.com>
2025-07-24 14:13:49 -0600

    adding of comment to codes

--- Admin_module.py
+++ Admin_module.py
@@ -7,6 +7,7 @@ driver = webdriver.Chrome()
 driver.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 driver.maximize_window()
 time.sleep(3)
+#login
 enter_username = driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input')
 enter_username.send_keys('Admin')
 enter_password = driver.find_element(By.CSS_SELECTOR, '#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div:nth-child(3) > div > div:nth-child(2) > input')
@@ -14,15 +15,24 @@ enter_password.send_keys('admin123')
 click_login_button = driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button')
 click_login_button.click()
 time.sleep(4)
+
+
+#Click Admin Module
 click_admin_button = driver.find_element(By.CSS_SELECTOR, '#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-navigation > aside > nav > div.oxd-sidepanel-body > ul > li:nth-child(1) > a > span')
 click_admin_button.click()
 time.sleep(3)
+
+#Click on User Management Button
 click_user_management_dropdown_button = driver.find_element(By.XPATH,'//*[@id="app"]/div[1]/div[1]/header/div[2]/nav/ul/li[1]/span')
 click_user_management_dropdown_button.click()
 time.sleep(2)
+
+#Click on Job Button
 click_job_dropdown_button = driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[1]/header/div[2]/nav/ul/li[2]/span')
 click_job_dropdown_button.click()
 time.sleep(2)
+
+#Click on Organization Button
 click_organization_dropdown_button = driver.find_element(By.CSS_SELECTOR,'#app > div.oxd-layout.orangehrm-upgrade-layout > div.oxd-layout-navigation > header > div.oxd-topbar-body > nav > ul > li:nth-child(3) > span')
 click_organization_dropdown_button.click()
 time.sleep(2)
